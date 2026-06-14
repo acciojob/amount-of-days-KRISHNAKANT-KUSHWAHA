c83bc73102bae383c5ab0a4627a0f4e35cac6069
Krishnakant Kushwaha <145730098+KRISHNAKANT-KUSHWAHA@users.noreply.github.com>
2026-06-15 02:31:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
 
 dayOfAYear( n){
-	if(n%4===0) return 366;
-	else if(n%100===0 && n%400===0) return 366;
-	else return 365;
+	if(n%400===0) return 366;
+	else if(n%100===0) return 365;
+	else if(n%4===0) return 366;
+	else return 365
 }
