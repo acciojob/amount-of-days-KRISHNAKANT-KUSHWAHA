7cdfd031ad340869386c0d5c66e49fa93e0b1d60
Krishnakant Kushwaha <145730098+KRISHNAKANT-KUSHWAHA@users.noreply.github.com>
2026-06-15 02:39:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-//your JS code here. If required.
-
-daysOfAYear( year){
-  if (year % 400 === 0) {
-    return 366;
-  } else if (year % 100 === 0) {
+function daysOfAYear(year) {
+    if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
+        return 366;
+    }
     return 365;
-  } else if (year % 4 === 0) {
-    return 366;
-  } else {
-    return 365;
-  }
 }
